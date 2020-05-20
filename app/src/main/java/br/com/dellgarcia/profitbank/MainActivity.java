2b224829fc8253dfd0b8346bbdb93b6d3f361c2e
Lucas Del Puerto <lucasgarciadragon@hotmail.com>
2020-05-20 11:42:27 -0300

    Initial commit

---
+++ app/src/main/java/br/com/dellgarcia/profitbank/MainActivity.java
@@ -0,0 +1,23 @@
+package br.com.dellgarcia.profitbank;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationMenuView navMenu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    }
+}
